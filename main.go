2fb42ab3b4338a017a23717b290334526f152760
Arshia Akhavan <letmemakenewone@gmail.com>
2020-07-26 22:33:46 +0430

    if runs currectly spawns some stderr

--- main.go
+++ main.go
@@ -11,23 +11,23 @@ import (
 )
 
 func main() {
-
 	stdinReader := bufio.NewReader(os.Stdin)
+
 	for {
 		fmt.Print("$> ")
 		input, err := stdinReader.ReadString('\n')
 		panicAttack("failed to read from input", err)
 		commandline := strings.Trim(string(input), " \n")
 		args := strings.Fields(commandline)
-
 		command := exec.Command(args[0], args[1:]...)
 		outStream, err := command.StdoutPipe()
 		errStream, err := command.StderrPipe()
 		command.Start()
 		output, err := ioutil.ReadAll(outStream)
 		errput, _ := ioutil.ReadAll(errStream)
+
 		command.Wait()
-		fmt.Println(string(errput))
+		log.Println(string(errput))
 		fmt.Println(string(output))
 	}
 }
