4df12267dd43eeed7d5dd5b727d28eae6ae653e6
Arshia Akhavan <letmemakenewone@gmail.com>
2020-07-27 21:50:42 +0430

    it works!

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"gong/command"
 	"gong/shell"
+	"os"
+	"strings"
 )
 
 func main() {
 	shell := shell.New()
+	shell.AddCommand(command.New("ls", func(args []string) {
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		fmt.Println(strings.Fields(input)[1])
+		fmt.Println("aliali")
+	}))
 	shell.Start()
 }
